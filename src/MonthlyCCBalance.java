660aba465ea9dcfa78a58e426af7201834b30eb0
dalto <dalton.m.towe@gmail.com>
2023-02-04 21:40:54 -0500

    wrote the base code

--- src/MonthlyCCBalance.java
+++ src/MonthlyCCBalance.java
@@ -1,6 +1,21 @@
-public class MonthlyCCBalance {
-    public static void main(String[] args) {
-        
+public class MonthlyCCBalance
+{
+    public static void main(String[] args)
+    {
+
+        double cCBalance = 5000;
+        double interestRate = .17;
+        double interestDueM1;
+        double interestDueM2;
+
+
+
+        interestDueM1 = cCBalance * interestRate;
+        cCBalance = cCBalance + interestDueM1;
+        interestDueM2 = (cCBalance * interestRate);
+
+        System.out.printf("Your credit card interest due month one is $%,.2f", + interestDueM1);
+        System.out.printf(" Month two interest due is $%,.2f", + interestDueM2);
     }
 
 }
